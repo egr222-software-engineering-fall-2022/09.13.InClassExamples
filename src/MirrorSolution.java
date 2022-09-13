d010f424aa19f986a0afc00826c962a061bd635d
Larry Clement <lclement@calbaptist.edu>
2022-09-12 21:33:13 -0700

    After adding solution

---
+++ src/MirrorSolution.java
@@ -0,0 +1,54 @@
+public class MirrorSolution {
+    public static final int SIZE = 4;
+
+    public static void main(String[] args) {
+        Line();
+        topHalf();
+        bottomHalf();
+        Line();
+    }
+
+    public static void topHalf() {
+        for (int line = 1; line <= SIZE; line++) {
+            System.out.print("|");
+            for (int space = 1; space <= ( line * -2 + (2 * SIZE)); space++) {
+                System.out.print(" ");  // note we used print and NOT println
+            }
+            System.out.print("<>");
+            for (int dot = 1; dot <= ( line * 4 - 4 ); dot++) {
+                System.out.print(".");
+            }
+            System.out.print("<>");
+            for (int space = 1; space <= ( line * -2 + (2 * SIZE)); space++) {
+                System.out.print(" ");  // note we used print and NOT println
+            }
+            System.out.println("|");
+        }
+    }
+
+    public static void bottomHalf() {
+        for (int line = SIZE; line >= 1; line--) {  // note:  this is the same as the topHalf but the loop runs backward
+            System.out.print("|");
+            for (int space = 1; space <= ( line * -2 + (2 * SIZE)); space++) {
+                System.out.print(" ");  // note we used print and NOT println
+            }
+            System.out.print("<>");
+            for (int dot = 1; dot <= ( line * 4 - 4 ); dot++) {
+                System.out.print(".");
+            }
+            System.out.print("<>");
+            for (int space = 1; space <= ( line * -2 + (2 * SIZE)); space++) {
+                System.out.print(" ");  // note we used print and NOT println
+            }
+            System.out.println("|");
+        }
+    }
+
+    public static void Line() {
+        System.out.print("#");
+        for (int i = 0; i < SIZE*4; i++) {  // Start at zero and make the test < SIZE * 4 [SIZE=3,12   =4, 16    =6,24]
+            System.out.print("=");
+        }
+        System.out.println("#");
+    }
+}
