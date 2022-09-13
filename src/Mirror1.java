db053750f0bd8002cb95a508f829054d694d498b
Larry Clement <lclement@calbaptist.edu>
2022-09-12 21:37:48 -0700

    After reinitializing Mirror1

--- src/Mirror1.java
+++ src/Mirror1.java
@@ -2,19 +2,5 @@ public class Mirror1 {
     public static final int SIZE = 4;
 
     public static void main(String[] args) {
-        topHalf();
-        bottomHalf();
-    }
-
-    public static void topHalf() {
-        for (int i = 1; i <= SIZE; i++) {
-
-        }
-    }
-
-    public static void bottomHalf() {
-        for (int i = 1; i <= SIZE; i++) {
-
-        }
     }
 }
