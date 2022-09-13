8b0640be24a44890c56fd2b773e10361b3839168
Larry Clement <lclement@calbaptist.edu>
2022-09-12 22:04:29 -0700

    After final update to MirrorSolution

--- src/MirrorSolution.java
+++ src/MirrorSolution.java
@@ -1,5 +1,5 @@
 public class MirrorSolution {
-    public static final int SIZE = 4;
+    public static final int SIZE = 6;
 
     public static void main(String[] args) {
         line();
@@ -10,38 +10,30 @@ public class MirrorSolution {
 
     public static void topHalf() {
         for (int line = 1; line <= SIZE; line++) {
-            System.out.print("|");
-            for (int space = 1; space <= ( line * -2 + (2 * SIZE)); space++) {
-                System.out.print(" ");  // note we used print and NOT println
-            }
-            System.out.print("<>");
-            for (int dot = 1; dot <= ( line * 4 - 4 ); dot++) {
-                System.out.print(".");
-            }
-            System.out.print("<>");
-            for (int space = 1; space <= ( line * -2 + (2 * SIZE)); space++) {
-                System.out.print(" ");  // note we used print and NOT println
-            }
-            System.out.println("|");
+            pattern(line);
         }
     }
 
     public static void bottomHalf() {
         for (int line = SIZE; line >= 1; line--) {  // note:  this is the same as the topHalf but the loop runs backward
-            System.out.print("|");
-            for (int space = 1; space <= ( line * -2 + (2 * SIZE)); space++) {
-                System.out.print(" ");  // note we used print and NOT println
-            }
-            System.out.print("<>");
-            for (int dot = 1; dot <= ( line * 4 - 4 ); dot++) {
-                System.out.print(".");
-            }
-            System.out.print("<>");
-            for (int space = 1; space <= ( line * -2 + (2 * SIZE)); space++) {
-                System.out.print(" ");  // note we used print and NOT println
-            }
-            System.out.println("|");
+            pattern(line);
         }
+    }
+
+    public static void pattern(int line) {
+        System.out.print("|");
+        for (int space = 1; space <= ( line * -2 + (2 * SIZE)); space++) {
+            System.out.print(" ");  // note we used print and NOT println
+        }
+        System.out.print("<>");
+        for (int dot = 1; dot <= ( line * 4 - 4 ); dot++) {
+            System.out.print(".");
+        }
+        System.out.print("<>");
+        for (int space = 1; space <= ( line * -2 + (2 * SIZE)); space++) {
+            System.out.print(" ");  // note we used print and NOT println
+        }
+        System.out.println("|");
     }
 
     public static void line() {
